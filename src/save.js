2ad79c42cd4209fe1def65896fa1502eaf42098d
rupesh20 <rupesh.patil741@gmail.com>
2018-04-11 18:41:13 +0530

    js

--- src/save.js
+++ src/save.js
@@ -1,20 +1,46 @@
 
-var btn = document.getElementById('bttn');
-var text = document.getElementById('input');
+var KeyPair = {};
+function setItem(){
+	console.log('ok');
+}
 
-function saveTabs(tabs,f){
-	for(let tab in tabs){
-		console.log(tab.url);
+
+function saveTabs(tabs){
+	
+	var arr = new Array;
+	let i=0;
+	//console.log(tabs[1].url);
+	while(i<tabs.length){
+		//console.log(tabs[i].url);
+		arr.push(tabs[i].url);
+		i+=1;	
 	}
+	flag=document.querySelector('#input').value
+	KeyPair[flag] = arr;
+	console.log(KeyPair);	
+	browser.storage.local.set({KeyPair}).then(setItem);	
 }
 
 function getTabs(){
-	return browser.tabs.query({currentWindow:true,active:true});
+	var tabList =  browser.tabs.query({currentWindow:true})
+	//console.log('in')
+	tabList.then(saveTabs)
+}
+
+function getList(){
+	var ul = document.getElementById("list");
+	
+	for(var key in KeyPair){
+  		var li = document.createElement("li");
+  		li.appendChild(document.createTextNode(key));
+  		ul.appendChild(li);
+	}		
 }
 
 function main(){
-	console.log('da');
-	var flag = txt.value;
-	getTabs().then(saveTabs,flag);
+	//console.log(flag)
+	getTabs()
+	getList()
 }
 
+document.querySelector('#bttn').addEventListener('click',main)
