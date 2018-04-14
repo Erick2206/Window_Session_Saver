6d8cb3ae744b27c49fb47692009976ca797e1922
rupesh20 <rupesh.patil741@gmail.com>
2018-04-14 12:13:42 +0530

    complete code

--- src/save.js
+++ src/save.js
@@ -1,8 +1,8 @@
 var KeyPair = {};
-
+var key;
 function reloadTabs(){
-	var key=this.id;
-	getList(key);
+	key=this.id;
+	browser.storage.local.get().then(getList);
 }
 //function to check the entry of data in storage
 function onCreated(windowInfo) {
@@ -26,14 +26,11 @@ function saveTabs(tabs){
 		i+=1;	
 	}
 
-	flag=document.querySelector('#input').value
+	flag=document.querySelector('#input').value;
+	//console.log(typeof(flag));
 	KeyPair[flag] = arr;	
 	browser.storage.local.set({KeyPair}).then(setItem);
-	browser.storage.local.get().then(
-		function(data){
-			console.log(data.KeyPair);
-		}
-	);	
+		
 }
 
 function getTabs(){
@@ -41,29 +38,41 @@ function getTabs(){
 	tabList.then(saveTabs)
 }
 
-function updateList(){
-	var ul = document.getElementById("list");
-	flag=document.querySelector('#input').value	
-	var btn = document.createElement("BUTTON");
-	
-	// code to add button with ID as Name entered by User
-	//  Onclick : Reload function is called to Load arrays
-	btn.appendChild(document.createTextNode(flag));
-	btn.id = flag;
-	btn.onclick = reloadTabs;
-	ul.appendChild(btn);
+function unique(flag){
 		
 }
 
-function getList(key){
-	browser.storage.local.get().then(
-		function(data){
-			var arr = new Array 
-			arr=data.KeyPair.key;
-			console.log(data.KeyPair);
+function updateList(){
+	var ul = document.getElementById("list");
+	flag=document.querySelector('#input').value
+	if(!document.getElementById(flag.valueOf())){	
+		var btn = document.createElement("BUTTON");
+		// code to add button with ID as Name entered by User
+		//  Onclick : Reload function is called to Load arrays
+		btn.appendChild(document.createTextNode(flag));
+		btn.id = flag;
+		btn.onclick = reloadTabs;
+		ul.appendChild(btn);
+	}	
+}
+
+function getList(data){
+	if(data.KeyPair){
+		try{
 			
+			obj = data.KeyPair;
+			console.log(key.valueOf());				
+			urlArr	= obj[key.valueOf()];
+			var creating = browser.windows.create({
+    				url: urlArr
+  			});
+  			creating.then(onCreated, onError);
 		}
-	);					
+		catch(e){
+			console.log(e);
+		}
+	}	
+						
 }
 
 function main(){
