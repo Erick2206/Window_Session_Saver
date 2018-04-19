5e50ec7e26b2432c3a08ec383fc4f363af10e0a1
Aayush Gogia <aayushar@yahoo.com>
2018-04-19 18:15:13 +0530

    added functionality to submit on pressing enter

--- src/save.js
+++ src/save.js
@@ -15,24 +15,24 @@ function onError(error) {
 function setItem(){
 	console.log('ok');
 }
-//Array of Tabs is stored as Key-Pair 
+//Array of Tabs is stored as Key-Pair
 function saveTabs(tabs){
-	
+
 	var arr = new Array;
 	let i=0;
-	
+
 	while(i<tabs.length){
-		if(tabs[i].url != ''){		
+		if(tabs[i].url != ''){
 			arr.push(tabs[i].url);
 			i+=1;
-		}	
+		}
 	}
 
 	flag=document.querySelector('#input').value;
 	//console.log(typeof(flag));
-	KeyPair[flag] = arr;	
+	KeyPair[flag] = arr;
 	browser.storage.local.set({KeyPair}).then(setItem);
-		
+
 }
 
 function getTabs(){
@@ -43,7 +43,7 @@ function getTabs(){
 function updateList(){
 	var ul = document.getElementById("list");
 	flag=document.querySelector('#input').value
-	if(!document.getElementById(flag.valueOf())){	
+	if(!document.getElementById(flag.valueOf())){
 		var btn = document.createElement("BUTTON");
 		// code to add button with ID as Name entered by User
 		//  Onclick : Reload function is called to Load arrays
@@ -51,15 +51,15 @@ function updateList(){
 		btn.id = flag;
 		btn.onclick = reloadTabs;
 		ul.appendChild(btn);
-	}	
+	}
 }
 
 function getList(data){
 	if(data.KeyPair){
 		try{
-			
+
 			obj = data.KeyPair;
-			//console.log(key.valueOf());				
+			//console.log(key.valueOf());
 			urlArr	= obj[key.valueOf()];
 			var creating = browser.windows.create({
     				url: urlArr
@@ -69,8 +69,8 @@ function getList(data){
 		catch(e){
 			console.log(e);
 		}
-	}	
-						
+	}
+
 }
 
 function main(){
@@ -79,3 +79,17 @@ function main(){
 }
 
 document.querySelector('#bttn').addEventListener('click',main)
+
+// Get the input field
+var input = document.getElementById("input");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Cancel the default action, if needed
+  event.preventDefault();
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Trigger the button element with a click
+    document.getElementById("bttn").click();
+  }
+});
