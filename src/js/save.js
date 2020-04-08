6a8731b02812a74ff70a4b8196b56d8523df74a2
Mridul Gogia <gogiamridul@gmail.com>
2020-04-08 18:59:41 +0530

    off for lunch

--- src/js/save.js
+++ src/js/save.js
@@ -1,10 +1,8 @@
-var refBtn = document.getElementsByClassName('btn-add');
-
+var refBtn = document.getElementsByClassName('btn-add')[0];
 
 var refLocalStorage = browser.storage.local;
 
 function isEmpty(obj) {
-	console.log('hereee');
     for(var key in obj) {
         if(obj.hasOwnProperty(key))
             return false;
@@ -12,28 +10,36 @@ function isEmpty(obj) {
     return true;
 }
 document.addEventListener('DOMContentLoaded', ()=>{
-	refBtn[0].addEventListener('click', ()=> {
-	console.log('statrt')
-	
+	refBtn.addEventListener('click', ()=> {
     browser.tabs.query({
         currentWindow: true
     })
     .then( tabs => {
         var arr =[];
         arr = tabs.map(item => item.url);
-        
+
         refLocalStorage.get()
             .then( tabsInLocalStorage => {
-                // var namesinLocalStorage = tabsInLocalStorage.keyPair;
-                var sessionName = document.getElementsByClassName('form-control').value;
+                const sessionName = document.getElementsByClassName('form-control')[0].value;
                 if(!isEmpty(tabsInLocalStorage)) {
-                    const flag = Object.keys(localStorage).filter(item => item === sessionName);
-                    if(flsag.length !== 0) {
-                      return  alert('Name already exists');
-                    } 
-                } 
-                refLocalStorage.set({sessionName: arr});    
-                console.log("done")    
+                  const flag = Object.keys(tabsInLocalStorage).filter( item => item === sessionName);
+                    if( flag.length > 0) {
+                      return  console.log('Name already exists');
+                    }
+                }
+                tabsInLocalStorage[sessionName] = arr;
+                refLocalStorage.set(tabsInLocalStorage)
+								.then(() => {
+                  const tabList = document.getElementsByClassName('tabList')[0];
+                  const button = document.createElement("BUTTON");
+                  button.classList.add("list-group-item");
+                  button.classList.add("list-group-item-action");
+                  button.setAttribute('type', "button");
+                    button.appendChild(document.createTextNode(sessionName));
+                    button.setAttribute('id', Object.keys(tabsInLocalStorage).length);
+                    // button.onclick = reloadTabs;
+                    tabList.appendChild(button);
+                });
             })
             .catch( err => console.log(err));
             })
@@ -41,4 +47,3 @@ document.addEventListener('DOMContentLoaded', ()=>{
 	}
 	);
 })
-
