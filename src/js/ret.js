71df535b8bc8f6d368946a15fa33b386febca0f3
Mridul Gogia <gogiamridul@gmail.com>
2020-04-11 18:20:09 +0530

    just had my coffee

--- src/js/ret.js
+++ src/js/ret.js
@@ -3,17 +3,17 @@ refLocalStorage.get()
         const tabList = document.getElementsByClassName('tabList')[0];
         if(!isEmpty(tabs)) {
             Object.keys(tabs).forEach( (item, index) => {
-              const button = document.createElement("BUTTON");
-              button.classList.add("list-group-item");
-              button.classList.add("list-group-item-action");
-              button.setAttribute('type', "button");
+                const button = document.createElement("BUTTON");
+                button.classList.add("list-group-item");
+                button.classList.add("list-group-item-action");
+                button.setAttribute('type', "button");
                 button.appendChild(document.createTextNode(item));
                 button.setAttribute('id', index);
-                // button.onclick = reloadTabs;
+                button.onclick = reloadTabs;
                 tabList.appendChild(button);
             })
         }else {
-            tabList.innerHtml = "No sessions saved";
+            tabList.innerHTML = `<h5 class="no_session">No sessions saved</h5>`;
         }
     })
 		.catch(err => console.log(err));
--- src/js/save.js
+++ src/js/save.js
@@ -1,6 +1,21 @@
 var refBtn = document.getElementsByClassName('btn-add')[0];
-
 var refLocalStorage = browser.storage.local;
+
+
+function reloadTabs() {
+    const value = this.innerHTML;    
+    refLocalStorage
+        .get()
+        .then(tabList => {
+            if(tabList) {
+                browser.windows.create({
+                    url: tabList[value]
+                })
+                    .catch(err => alert("There are some invalid URL's"));
+            }
+        })
+        .catch(err => alert("There are some invalid URL's"));
+}
 
 function isEmpty(obj) {
     for(var key in obj) {
@@ -19,25 +34,27 @@ document.addEventListener('DOMContentLoaded', ()=>{
         arr = tabs.map(item => item.url);
 
         refLocalStorage.get()
-            .then( tabsInLocalStorage => {
+            .then( savedTabs => {
                 const sessionName = document.getElementsByClassName('form-control')[0].value;
-                if(!isEmpty(tabsInLocalStorage)) {
-                  const flag = Object.keys(tabsInLocalStorage).filter( item => item === sessionName);
+                if(!isEmpty(savedTabs)) {
+                  const flag = Object.keys(savedTabs).filter( item => item === sessionName);
                     if( flag.length > 0) {
                       return  console.log('Name already exists');
                     }
+                } else {
+                    document.getElementsByClassName('tabList')[0].innerHTML = '';
                 }
-                tabsInLocalStorage[sessionName] = arr;
-                refLocalStorage.set(tabsInLocalStorage)
-								.then(() => {
-                  const tabList = document.getElementsByClassName('tabList')[0];
-                  const button = document.createElement("BUTTON");
-                  button.classList.add("list-group-item");
-                  button.classList.add("list-group-item-action");
-                  button.setAttribute('type', "button");
+                savedTabs[sessionName] = arr;
+                refLocalStorage.set(savedTabs)
+                                .then(() => {
+                    const tabList = document.getElementsByClassName('tabList')[0];
+                    const button = document.createElement("BUTTON");
+                    button.classList.add("list-group-item");
+                    button.classList.add("list-group-item-action");
+                    button.setAttribute('type', "button");
                     button.appendChild(document.createTextNode(sessionName));
-                    button.setAttribute('id', Object.keys(tabsInLocalStorage).length);
-                    // button.onclick = reloadTabs;
+                    button.setAttribute('id', Object.keys(savedTabs).length);
+                    button.onclick = reloadTabs;
                     tabList.appendChild(button);
                 });
             })
